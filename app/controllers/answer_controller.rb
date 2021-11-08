226a995eed2e28fc0edbbcd67fbe22f85f06ab30
Jonas Stannowski <jonas.stannowski@icloud.com>
2021-11-08 15:28:29 +0100

    finished challenge

---
+++ app/controllers/answer_controller.rb
@@ -0,0 +1,11 @@
+class AnswerController < ApplicationController
+  def answer
+    if params[:question] == "I am going to work"
+      @answer = "Great!"
+    elsif params[:question].end_with? "?"
+      @answer = "silly question, get dressed and go to work!"
+    else
+      @answer = "I don´t care, get dressed and go to work"
+    end
+  end
+end
